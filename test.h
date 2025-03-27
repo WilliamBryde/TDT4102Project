1406b6ee84e899993f644286e3a8057dc691c1d8
WilliamBryde <williambryde@gmail.com>
2025-03-27 13:57:27 +0100

    En slags start

---
+++ test.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class ovelse{
+    std::string navn;
+    std::vector<int> reps;
+    std::vector<int> vekt;
+    
+};
